555c9e2fa6bcb8ade672346672ca20acaff47196
Stefan Englmeier <stefanenglmeier@gmail.com>
2018-07-25 23:12:58 +0200

    cleanup

--- input.py
+++
@@ -1,6 +0,0 @@
-class input:
-    def __init__(self, transacion, position):
-        self.transacion = transacion
-        self.position = position
-    def toString(self):
-        return "a"
--- output.py
+++
@@ -1,7 +0,0 @@
-class output:
-    def __init__(self, value, reciver):
-        self.value = value
-        self.reciver = reciver
-
-    def toString(self):
-        return "a"
